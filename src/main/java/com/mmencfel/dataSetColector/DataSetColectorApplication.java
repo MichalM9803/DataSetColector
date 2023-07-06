f7c40ac7f85cc7da9c7d7920eadc4628ac3111a1
Michal Mencfel <269996@student.pwr.edu.pl>
2023-07-06 22:25:20 +0200

    initial commit

---
+++ src/main/java/com/mmencfel/dataSetColector/DataSetColectorApplication.java
@@ -0,0 +1,13 @@
+package com.mmencfel.dataSetColector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSetColectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSetColectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmencfel/dataSetColector/DataSetColectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmencfel.dataSetColector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSetColectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
